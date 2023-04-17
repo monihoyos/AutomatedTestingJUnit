49dc9be68b336ce410ab0492ad3e3173c3ed20d1
Monica Hoyos <moni.hoyosg@gmail.com>
2023-04-17 16:58:52 -0500

    Add multiply method

--- src/main/java/automated/testing/maven/calculatorProject/Calculator.java
+++ src/main/java/automated/testing/maven/calculatorProject/Calculator.java
@@ -15,4 +15,7 @@ public class Calculator {
     public int subtract (int num1, int num2 ){
         return num1 - num2;
     }
+    public double multiply (double num1, double num2){
+        return num1*num2;
+    }
 }
