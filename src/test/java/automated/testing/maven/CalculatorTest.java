03c5846983c0881f3bc76576ffb180b22475f8d2
Monica Hoyos <moni.hoyosg@gmail.com>
2023-04-17 16:50:29 -0500

    Implementation of xray annotations

--- src/test/java/automated/testing/maven/CalculatorTest.java
+++ src/test/java/automated/testing/maven/CalculatorTest.java
@@ -1,5 +1,7 @@
 package automated.testing.maven;
 
+import app.getxray.xray.testng.annotations.Requirement;
+import app.getxray.xray.testng.annotations.XrayTest;
 import automated.testing.maven.calculatorProject.Calculator;
 import io.qameta.allure.Allure;
 import org.testng.Assert;
@@ -16,23 +18,31 @@ public class CalculatorTest {
     }
 
     @Test
+    @XrayTest(key = "TNP-5", summary = "addition of two numbers", description = "tests the sum of two positive integers", labels = "Calculator, sum")
+    @Requirement(key = "TNP-12")
     public void addTwoNumbers(){
         Allure.step("Doing checks when add 2 numbers");
         Assert.assertEquals(20,calc.add(10,10));
     }
 
+    @XrayTest(key = "TNP-6", summary = "addition of three numbers", description = "tests the sum of three positive integers", labels = "Calculator, sum")
+    @Requirement(key = "TNP-12")
     @Test
     public void addThreeNumbers(){
         Allure.step("Doing checks when add 3 numbers");
         Assert.assertEquals(20,calc.add(5,5,10));
     }
 
+    @XrayTest(key = "TNP-7", summary = "Subtraction of two numbers", description = "tests the subtract of two positive integers", labels = "Calculator, subtract")
+    @Requirement(key = "TNP-12")
     @Test
     public void subtractTwoNumbers(){
         Allure.step("Doing checks when subtract 2 numbers");
         Assert.assertEquals(5,calc.subtract(10,5));
     }
 
+    @XrayTest(key = "TNP-8", summary = "Subtraction of three numbers", description = "tests the subtract of two positive integers", labels = "Calculator, subtract")
+    @Requirement(key = "TNP-12")
     @Test
     public void subtractThreeNumbers() {
         Allure.step("Doing checks when subtract 3 numbers");
