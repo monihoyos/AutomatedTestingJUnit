71456113e47d197fa54936b70c58f72b11bf1667
Monica Hoyos <moni.hoyosg@gmail.com>
2023-04-17 11:02:13 -0500

    Add allure step on test in CalculatorTest class

--- src/test/java/automated/testing/maven/CalculatorTest.java
+++ src/test/java/automated/testing/maven/CalculatorTest.java
@@ -1,6 +1,8 @@
 package automated.testing.maven;
 
 import automated.testing.maven.calculatorProject.Calculator;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
@@ -16,21 +18,26 @@ public class CalculatorTest {
 
     @Test
     public void addTwoNumbers(){
+        Allure.step("Doing checks when add 2 numbers");
         Assert.assertEquals(20,calc.add(10,10));
     }
 
     @Test
     public void addThreeNumbers(){
+        Allure.step("Doing checks when add 3 numbers");
         Assert.assertEquals(20,calc.add(5,5,10));
     }
 
     @Test
     public void subtractTwoNumbers(){
+        Allure.step("Doing checks when subtract 2 numbers");
         Assert.assertEquals(5,calc.subtract(10,5));
     }
 
     @Test
-    public void subtractThreeNumbers(){
+    public void subtractThreeNumbers() {
+        Allure.step("Doing checks when subtract 3 numbers");
         Assert.assertEquals(5,calc.subtract(20,10,5));
     }
+
 }
