a963702ba43e67b1cf45b63a7b5bb2fb62290478
Monica Hoyos <moni.hoyosg@gmail.com>
2023-04-17 17:10:54 -0500

    Remove summary, description and labels attributes
    Summary and description passed in the attributes are not yet supported in Cloud version of Xray.

--- src/test/java/automated/testing/maven/CalculatorTest.java
+++ src/test/java/automated/testing/maven/CalculatorTest.java
@@ -17,32 +17,36 @@ public class CalculatorTest {
         calc=new Calculator();
     }
 
+  /*  @XrayTest(key = "TNP-5", summary = "addition of two numbers", description = "tests the sum of two positive integers", labels = "Calculator, sum")
+    Summary and description passed in the attributes are not yet supported in Cloud version of Xray.
+    https://docs.getxray.app/display/XRAYCLOUD/Testing+web+applications+using+Selenium+and+TestNG+in+Java*/
+
     @Test
-    @XrayTest(key = "TNP-5", summary = "addition of two numbers", description = "tests the sum of two positive integers", labels = "Calculator, sum")
-    @Requirement(key = "TNP-12")
+    @XrayTest(key="TNP-5")
+    @Requirement(key="TNP-12")
     public void addTwoNumbers(){
         Allure.step("Doing checks when add 2 numbers");
         Assert.assertEquals(20,calc.add(10,10));
     }
 
-    @XrayTest(key = "TNP-6", summary = "addition of three numbers", description = "tests the sum of three positive integers", labels = "Calculator, sum")
-    @Requirement(key = "TNP-12")
+    @XrayTest(key="TNP-6")
+    @Requirement(key="TNP-12")
     @Test
     public void addThreeNumbers(){
         Allure.step("Doing checks when add 3 numbers");
         Assert.assertEquals(20,calc.add(5,5,10));
     }
 
-    @XrayTest(key = "TNP-7", summary = "Subtraction of two numbers", description = "tests the subtract of two positive integers", labels = "Calculator, subtract")
-    @Requirement(key = "TNP-12")
+    @XrayTest(key="TNP-7")
+    @Requirement(key="TNP-12")
     @Test
     public void subtractTwoNumbers(){
         Allure.step("Doing checks when subtract 2 numbers");
         Assert.assertEquals(5,calc.subtract(10,5));
     }
 
-    @XrayTest(key = "TNP-8", summary = "Subtraction of three numbers", description = "tests the subtract of two positive integers", labels = "Calculator, subtract")
-    @Requirement(key = "TNP-12")
+    @XrayTest(key="TNP-8")
+    @Requirement(key="TNP-12")
     @Test
     public void subtractThreeNumbers() {
         Allure.step("Doing checks when subtract 3 numbers");
@@ -50,8 +54,8 @@ public class CalculatorTest {
     }
 
     @Test
-    @XrayTest( summary = "Multiplycation of two numbers", description = "tests the multiply of two positive integers", labels = "Calculator, multiply")
-    @Requirement(key = "TNP-12")
+    @XrayTest(key="TNP-15")
+    @Requirement(key="TNP-12")
     public void multiplyTwoNumbers(){
         Allure.step("Doing checks when add 2 numbers");
         Assert.assertEquals(100,calc.multiply(10,10));
